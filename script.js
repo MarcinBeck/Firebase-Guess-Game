1970a37d778da38a136389ae6e355018de6822d2
MarcinBeck <marcin.beck@gmail.com>
2025-08-22 14:01:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "TWOJ_API_KEY",
-    authDomain: "guess-5d206.firebaseapp.com",
-    projectId: "guess-5d206",
-    storageBucket: "guess-5d206.appspot.com",
-    messagingSenderId: "TWOJ_MESSAGING_SENDER_ID",
-    appId: "TWOJ_APP_ID"
+  apiKey: "AIzaSyDgnmnrBiqwFuFcEDpKsG_7hP2c8C4t30E",
+  authDomain: "guess-5d206.firebaseapp.com",
+  projectId: "guess-5d206",
+  storageBucket: "guess-5d206.firebasestorage.app",
+  messagingSenderId: "88077227103",
+  appId: "1:88077227103:web:4eae99db4fb7e1fcbcb7ea",
+  measurementId: "G-V620WE8CYB"
 };
 
 // Zastąp "TWOJ_API_KEY" i pozostałe wartości swoimi danymi z konsoli Firebase
@@ -34,3 +35,4 @@ testConnection();
 
 // WAŻNE: Cały Twój właściwy kod (z klasyfikatorem) tymczasowo usuń
 // Wklej to w jego miejsce
+
